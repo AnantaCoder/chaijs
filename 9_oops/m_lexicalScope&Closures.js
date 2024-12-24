588977778057188dda9443ca7884c1d29371c12e
Anirban Sarkar <anirbansarkar.slg18@gmail.com>
2024-12-24 14:36:09 +0530

    f

--- 9_oops/m_lexicalScope&Closures.js
+++ 9_oops/m_lexicalScope&Closures.js
@@ -1,3 +1,13 @@
 //closures:
 //  where an inner function "remembers" variables from 
 // its outer function's scope even after the outer function has finished executing.
+
+function init() {
+    var name = "Mozilla"; // name is a local variable created by init
+    function displayName() {
+        // displayName() is the inner function, that forms a closure
+        console.log(name); // use variable declared in the parent function
+    }
+    displayName();
+}
+init();
